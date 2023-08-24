004500a8741a761df7f2459cc390b0a5dce05e58
Kevin Anadon <kevinxd77200@gmail.com>
2023-08-24 09:51:34 -0300

    refactor: name of property of response in the create-room endpoint

--- server/src/controllers/create-room.ts
+++ server/src/controllers/create-room.ts
@@ -2,5 +2,5 @@ import { Request, Response } from "express"
 import { v4 as uuidV4 } from "uuid"
 
 export const createRoom = (req: Request, res: Response) => {
-  res.status(200).json({ link: uuidV4() })
+  res.status(200).json({ roomID: uuidV4() })
 }
