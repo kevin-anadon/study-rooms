e133fc4f08a57f133b9e860d9694b381e14ab7d4
Mayza Ferreira <mayza_ferr@hotmail.com>
2024-01-16 21:01:34 -0300

    Feature: homepage background video

--- client/src/components/JoinButton.jsx
+++ client/src/components/JoinButton.jsx
@@ -2,7 +2,7 @@ import React from "react";
 import "./Button.css";
 export default function join() {
   return (
-    <a href="" className="btnRoom btn  m-5 ">
+    <a href="" className="btnRoom btn  m-2 ">
       Join room
     </a>
   );
--- client/src/pages/Home/Home.jsx
+++ client/src/pages/Home/Home.jsx
@@ -1,18 +1,20 @@
 import React from "react";
 import "bootstrap/dist/css/bootstrap.min.css";
 import "./Home.css";
-
-import Navbar from "../../components/Navbar.jsx";
+import homeBackgroundVideo from "../../assets/videos/homeBackgroundVideo.mp4";
 import CreateButton from "../../components/CreateButton.jsx";
 import JoinButton from "../../components/JoinButton.jsx";
 
 export default function Home() {
   return (
-    <div>
-      <Navbar />
-      <h1 className="tittle">Study rooms</h1>
-      <CreateButton />
-      <JoinButton />
+    <div className="background">
+      <div className="overlay"></div>
+      <video src={homeBackgroundVideo} autoPlay loop muted />
+      <div className="content">
+        <h1 className="tittle">Study rooms</h1>
+        <CreateButton />
+        <JoinButton />
+      </div>
     </div>
   );
 }
